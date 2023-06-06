92eb67146f5292d1350f228ea9e7c4df2c563bf6
kmoilane <karri.moilanen@gmail.com>
2023-06-06 16:02:32 +0300

    Created struct for book

---
+++ week1/tuesday/Assignment_1_2_5/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+struct Book
+{
+    std::string title {};
+    std::string author {};
+    long long   isbn {};
+    int         year {};
+    int         quanityty {};
+};
