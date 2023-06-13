280ad85d173e56f3edcc206a0fcf34dc56f0c207
kmoilane <karri.moilanen@gmail.com>
2023-06-13 17:28:14 +0300

    Refactoring the code to multiple source and header files

--- week2/d02/Assignment_2_2_3/main.cpp
+++ week2/d02/Assignment_2_2_3/main.cpp
@@ -1,170 +1,17 @@
+#include "includes/board.h"
+#include "includes/player.h"
+#include "includes/utils.h"
 #include <iostream>
 #include <vector>
 #include <random>
 #include <chrono>
 
-using Matrix = std::vector<std::vector<char>>;
-
-void clear_cin()
+bool victory(Player& player)
 {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-struct Cell
-{
-    char    obstacle { 'O' };
-    char    free { ' ' };
-    char    player { 'A' };
-
-    char random()
-    {
-        std::random_device rd{};
-        std::seed_seq ss{
-			static_cast<std::seed_seq::result_type>
-            (std::chrono::steady_clock::now().time_since_epoch().count()),
-			rd(), rd(), rd(), rd(), rd(), rd(), rd() };
-        std::mt19937 mt {ss};
-        std::uniform_int_distribution distr{1, 5};
-        int res = distr(mt);
-        if(res == 1)
-            return obstacle;
-        return free;
-    }
-} cell;
-
-struct Player
-{
-    int row { 0 };
-    int col { 0 };
-    int points { 0 };
-};
-
-void init_board(Matrix& map)
-{
-    for (int i = 0; i < 10; ++i)
-    {
-        std::vector<char> row {};
-        for (int j = 0; j < 10; ++j)
-        {
-            if (i == 0 && j == 0)
-            {
-                row.push_back(cell.player);
-                continue ;
-            }
-            row.push_back(cell.random());
-        }
-        map.push_back(row);
-    }
-}
-
-void draw_map(Matrix& map, Player& player){
-    for (int i = 0; i < 10; ++i)
-    {
-        for (int j = 0; j < 10; ++j)
-        {
-            std::cout << map[i][j];
-        }
-        std::cout << '\n';
-    }
-}
-
-void print_options()
-{
-    std::cout << "\nw) Move north\n";
-    std::cout << "a) Move west\n";
-    std::cout << "s) Move south\n";
-    std::cout << "d) Move east\n";
-    std::cout << "q) Quit\n";
-}
-
-bool check_north(Matrix& map, Player& player){
-    if (player.row != 0)
-    {
-        if (map[player.row - 1][player.col] != cell.obstacle)
-            return true;
-    }
+    if (player.col == 9 && player.points >= 5)
+        return true;
     return false;
 }
-
-bool check_west(Matrix& map, Player &player){
-    if (player.col != 0)
-    {
-        if (map[player.row][player.col - 1] != cell.obstacle)
-            return true;
-    }
-    return false;
-}
-
-bool check_south(Matrix& map, Player& player){
-    if (player.row != 9)
-    {
-        if (map[player.row + 1][player.col] != cell.obstacle)
-            return true;
-    }
-    return false;
-}
-
-bool check_east(Matrix& map, Player& player){
-    if (player.col != 9)
-    {
-        if (map[player.row][player.col + 1] != cell.obstacle)
-            return true;
-    }
-    return false;
-}
-
-/*
-**  Handles player movement based on 4 directions
-*/
-void move_player(char dir, Matrix& map, Player& player)
-{
-    if (dir == 'w')
-    {
-        if (check_north(map, player))
-        {
-            std::cout << "\nPlayer moves north!\n";
-            map[player.row][player.col] = cell.free;
-            player.row -= 1;
-        }
-        else
-            std::cout << "\nPlayer can't move north!\n";
-    }
-    else if (dir == 'a')
-    {
-        if (check_west(map, player))
-        {
-            std::cout << "\nPlayer moves west!\n";
-            map[player.row][player.col] = cell.free;
-            player.col -= 1;
-        }
-        else
-            std::cout << "\nPlayer can't move west!\n";
-    }
-    else if (dir == 's')
-    {
-        if (check_south(map, player))
-        {
-            std::cout << "\nPlayer moves south!\n";
-            map[player.row][player.col] = cell.free;
-            player.row += 1;
-        }
-        else
-            std::cout << "\nPlayer can't move south!\n";
-    }
-    else if (dir == 'd')
-    {
-        if (check_east(map, player))
-        {
-            std::cout << "\nPlayer moves east!\n";
-            map[player.row][player.col] = cell.free;
-            player.col += 1;
-        }
-        else
-            std::cout << "\nPlayer can't move east!\n";
-    }
-}
-
 
 /*
 **  Main game loop
@@ -172,14 +19,20 @@ void move_player(char dir, Matrix& map, Player& player)
 int main()
 {
     Matrix map {};
-
     Player player {};
+    Cell cell {};
 
-    init_board(map);
+    init_board(map, cell);
+    add_treasure(map, cell);
     
     while (true)
     {
-        draw_map(map, player);
+        draw_map(map, player, cell);
+        if (victory(player))
+        {
+            std::cout << "\n\nYOU WON!\n\n";
+            return 0;
+        }
         print_options();
         char option {};
         std::cin >> option;
@@ -191,7 +44,7 @@ int main()
         else if (option == 'w' || option == 'a'
             || option == 's' || option == 'd')
         {
-            move_player(option, map, player);
+            move_player(option, map, player, cell);
         }
         else
         {
