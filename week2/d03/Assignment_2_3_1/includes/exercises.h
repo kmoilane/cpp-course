a7d5b3dc3df5b00b0b82a836f73937e3ea8c4f0b
kmoilane <karri.moilanen@gmail.com>
2023-06-14 09:42:45 +0300

    Exercise 1 and modified file structure and naming

---
+++ week2/d03/Assignment_2_3_1/includes/exercises.h
@@ -0,0 +1,10 @@
+#ifndef EXERCISES_H
+#define EXERCISES_H
+
+#include <vector>
+
+void sort_int_vector(int arr[]);
+void ex1_main();
+void print_vect(std::vector<int>& vect);
+
+#endif
--- week2/d03/Assignment_2_3_1/main.cpp
+++ week2/d03/Assignment_2_3_1/main.cpp
@@ -1,3 +1,22 @@
+#include "includes/exercises.h"
+#include <algorithm>
 #include <iostream>
 
+void print_vect(std::vector<int>& vect)
+{
+    for (auto i : vect)
+    {
+        std::cout << i;
+        if (i != vect.back())
+            std::cout << ", ";
+    }
+    std::cout << '\n';
+}
 
+int main()
+{
+    ex1_main();
+
+
+    return 0;
+}
