7b5bbe8878c0d458b578b69e223e61bdcd3012e9
kmoilane <karri.moilanen@gmail.com>
2023-06-15 13:28:16 +0300

    added externals folder to gitignore

--- week2/d04/Assignment_2_4_1/includes/line_count.h
+++ week2/d04/Assignment_2_4_1/includes/line_count.h
@@ -1,6 +1,7 @@
 #ifndef LINE_COUNT_H
 #define LINE_COUNT_H
 
+#include "word_count.h"
 #include <string>
 
 int print_file_line_count(const std::string & file_name);
--- week2/d04/Assignment_2_4_1/includes/word_count.h
+++ week2/d04/Assignment_2_4_1/includes/word_count.h
@@ -1,6 +1,7 @@
 #ifndef WORD_COUNT_H
 #define WORD_COUNT_H
 
+#include "line_count.h"
 #include <string>
 
 int print_file_word_count(const std::string & file_name);
