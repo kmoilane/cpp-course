43ce8b1e4bc3cba262266c585ada4e3a1f9bd5dc
kmoilane <karri.moilanen@gmail.com>
2023-06-21 13:59:53 +0300

    unit testing with simple math and string functions

---
+++ week3/d03/Assignment_3_3_2/includes/math_functions.h
@@ -0,0 +1,13 @@
+#ifndef MATH_FUNCTIONS_H
+#define MATH_FUNCTIONS_H
+
+int addition(int a, int b);
+
+int substraction(int a, int b);
+
+int multiplication(int a, int b);
+
+int division(int a, int b);
+
+#endif
+
