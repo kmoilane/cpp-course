c97dedc285e3a6026e9d587cd962249bc93b9a58
kmoilane <karri.moilanen@gmail.com>
2023-07-20 12:45:03 +0300

    Added integer overflow checks

--- week6/d04/Assignment_6_4_1/includes/point_3d.hpp
+++ week6/d04/Assignment_6_4_1/includes/point_3d.hpp
@@ -1,12 +1,18 @@
 #ifndef EX2_HPP
 #define EX2_HPP
 #include <iostream>
+#include <cstdlib>
+#include <limits>
 
 namespace myspace
 {
     class Point_3D
     {
         public:
+            int x;
+            int y;
+            int z;
+            Point_3D() = default;
             Point_3D(int a, int b, int c) : x(a), y(b), z(c){};
             friend std::ostream& operator<<(std::ostream& os, const Point_3D& vect);
             friend Point_3D operator+(const Point_3D& v1, const Point_3D& v2);
@@ -14,9 +20,7 @@ namespace myspace
             Point_3D& operator+=(const Point_3D& v2);
             Point_3D& operator-=(const Point_3D& v2);
         private:
-            int x;
-            int y;
-            int z;
+            const int legal_int(const int64_t& val) const;
     };
 
     /*
@@ -27,23 +31,54 @@ namespace myspace
         return os;
     }
     Point_3D& Point_3D::operator+=(const Point_3D& v2){
-        x += v2.x;
-        y += v2.y;
-        z += v2.z;
+        x = Point_3D::legal_int(static_cast<int64_t>(x) +
+                                static_cast<int64_t>(v2.x));
+        y = Point_3D::legal_int(static_cast<int64_t>(y) +
+                                static_cast<int64_t>(v2.y));
+        z = Point_3D::legal_int(static_cast<int64_t>(z) +
+                                static_cast<int64_t>(v2.z));
         return *this;
     }
     Point_3D& Point_3D::operator-=(const Point_3D& v2){
-        x -= v2.x;
-        y -= v2.y;
-        z -= v2.z;
+        x = Point_3D::legal_int(static_cast<int64_t>(x) -
+                                static_cast<int64_t>(v2.x));
+        y = Point_3D::legal_int(static_cast<int64_t>(y) -
+                                static_cast<int64_t>(v2.y));
+        z = Point_3D::legal_int(static_cast<int64_t>(z) -
+                                static_cast<int64_t>(v2.z));
         return *this;
     }
     Point_3D operator+(const Point_3D& v1, const Point_3D& v2){
-        return {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+        Point_3D point;
+        return {point.legal_int(static_cast<int64_t>(v1.x) +
+                                static_cast<int64_t>(v2.x)),
+                point.legal_int(static_cast<int64_t>(v1.y) +
+                                static_cast<int64_t>(v2.y)),
+                point.legal_int(static_cast<int64_t>(v1.z) +
+                                static_cast<int64_t>(v2.z))};
     }
     Point_3D operator-(const Point_3D& v1, const Point_3D& v2){
-        return {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+        Point_3D point;
+        return {point.legal_int(static_cast<int64_t>(v1.x) -
+                                static_cast<int64_t>(v2.x)),
+                point.legal_int(static_cast<int64_t>(v1.y) -
+                                static_cast<int64_t>(v2.y)),
+                point.legal_int(static_cast<int64_t>(v1.z) -
+                                static_cast<int64_t>(v2.z))};
+    }
+
+    /*
+    **  If val is over max int size, returns max int, if under min int size
+    **  returns min int, else returns val
+    */
+     const int Point_3D::legal_int(const int64_t& val) const {
+        if (val >= std::numeric_limits<int>::max())
+            return std::numeric_limits<int>::max();
+        else if (val <= std::numeric_limits<int>::min())
+            return std::numeric_limits<int>::min();
+        return static_cast<int>(val);      
     }
 }
+
 
 #endif
--- week6/d04/Assignment_6_4_1/point_3d.cpp
+++ week6/d04/Assignment_6_4_1/point_3d.cpp
@@ -3,9 +3,13 @@
 
 int main()
 {
+    int max = std::numeric_limits<int>::max();
+    int min =std::numeric_limits<int>::min();
     myspace::Point_3D v1{5, 6, 7};
     myspace::Point_3D v2{1, 1, 1};
     myspace::Point_3D v3{3, 3, 3};
+    myspace::Point_3D v4{max, max, max};
+    myspace::Point_3D v5{min, min, min};
     std::cout << v1;
     std::cout << v1 + v2;
     std::cout << v2;
@@ -14,5 +18,10 @@ int main()
     std::cout << (v3 -= v2);
     std::cout << v3 - v1;
     std::cout << v3;
+    std::cout << v4;
+    std::cout << v5;
+    std::cout << (v4 + v2);
+    std::cout << (v5 -= v5);
+    std::cout << v5 - v4 - v4;
     return 0;
 }
