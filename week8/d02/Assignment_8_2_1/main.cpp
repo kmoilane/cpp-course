0390aa51c8dbca328b7c6369f2756db553216ee9
kmoilane <karri.moilanen@gmail.com>
2023-08-01 14:06:02 +0300

    Exercise 3

--- week8/d02/Assignment_8_2_1/main.cpp
+++ week8/d02/Assignment_8_2_1/main.cpp
@@ -1,3 +1,4 @@
+#include "includes/counter.hpp"
 #include "includes/debug.hpp"
 #include <iostream>
 #include <type_traits>
@@ -14,22 +15,38 @@ void print_valueness(T&& val)
 int main()
 {
     int x = 42;
-    std::cout << "Exercise 1\n";
+    std::cout << "\033[1;34mEXERCISE 1\033[0m\n";
     print_valueness(x);
     print_valueness(std::move(x));
     
-    std::cout << "\nExercise 2\n";
-    Debug<true> bug1;
-    Debug<true> bug2(bug1);
-    Debug<true> bug4;
-    Debug<true> bug3(std::move(bug2));
+    std::cout << "\033[1;34mEXERCISE 2\033[0m\n";
     {
+        Debug<true> bug1;
+        Debug<true> bug2(bug1);
+        Debug<true> bug4;
+        Debug<true> bug3(std::move(bug2));
         Debug<false> bug5;
     }
     std::cout << "Default constructor called: " << Debug<true>::constructor_count << '\n';
     std::cout << "Default destructor called: " << Debug<true>::destructor_count << '\n';
     std::cout << "Move constructor called: " << Debug<true>::move_count << '\n';
     std::cout << "Copy constructor called: " << Debug<true>::copy_count << '\n';
+
+    std::cout << "\033[1;34mEXERCISE 3\033[0m\n";
+    Counter c1;
+    std::cout << "instances: 1 = " << Counter::get_count() << '\n';
+    {
+        Counter c3;
+        std::cout << "instances: 2 = " << Counter::get_count() << '\n';
+        {
+            Counter c4(std::move(c3));
+            std::cout << "instances: 2 = " << Counter::get_count() << '\n';
+            Counter c5(std::move(c4));
+            std::cout << "instances: 2 = " << Counter::get_count() << '\n';
+        }
+        std::cout << "instances: 1 = " << Counter::get_count() << '\n';
+    }
+    std::cout << "instances: 1 = " << Counter::get_count() << '\n';
 
     return 0;
 }
