3dbe968017f73d6fda87598057228455b04f0556
kmoilane <karri.moilanen@gmail.com>
2023-08-03 16:49:27 +0300

    Ex2

--- week8/d04/Assignnment_8_4_1/tests/test.cpp
+++ week8/d04/Assignnment_8_4_1/tests/test.cpp
@@ -1,6 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "includes/doctest.h"
 #include "../includes/ex1.hpp"
+#include "../includes/ex2.hpp"
+#include <string>
 
 TEST_CASE("EX1 Tests")
 {
@@ -26,5 +28,14 @@ TEST_CASE("EX1 Tests")
         CHECK(object_storage[1].name == std::string("object number 0"));
         large_object l3;
         CHECK(object_storage[2].name == std::string("object number 2"));
+    }
+}
+
+TEST_CASE("EX2 Tests")
+{
+    SUBCASE("Testing non trivial type std::string")
+    {
+        flawed_array<std::string, 2> farray;
+        CHECK(farray.size() == 2);
     }
 }
